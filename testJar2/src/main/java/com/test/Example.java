fe97ef975d4be5c887a70fabf614c31e0056114d
boronga <boronga.jio@gmail.com>
2021-11-24 17:42:11 +0800

    init

---
+++ testJar2/src/main/java/com/test/Example.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * @author boronga
+ * @ClassName Example
+ * @description todo
+ * @data 2021/11/24
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(StrUtil.isNotBlank(""));
+    }
+}
